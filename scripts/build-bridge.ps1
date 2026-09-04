e7b841a533f578ac5d55f154147ea48e80298544
harminoff <harminoff@gmail.com>
2026-09-04 14:19:59 -0500

    ci: force MSYS shell for bridge build

--- scripts/build-bridge.ps1
+++ scripts/build-bridge.ps1
@@ -6,13 +6,15 @@ $projectRoot = Get-ProjectBroomRoot
 $sourceRoot = Join-Path $projectRoot "src\brogue-mapgen"
 $make = Resolve-ProjectBroomMsysTool "make.exe"
 $gcc = Resolve-ProjectBroomMsysTool "x86_64-w64-mingw32-gcc.exe"
-Enable-ProjectBroomMsysPath @($make, $gcc)
+$sh = Resolve-ProjectBroomMsysTool "sh.exe"
+Enable-ProjectBroomMsysPath @($make, $gcc, $sh)
 $gccFlavor = Split-Path -Leaf (Split-Path -Parent (Split-Path -Parent $gcc))
 $makePath = "PATH=/usr/bin:/$gccFlavor/bin:/bin"
+$makeShell = $sh.Replace('\', '/')
 
 Push-Location $sourceRoot
 try {
-    & $make $makePath SYSTEM=WINDOWS GRAPHICS=NO RELEASE=YES BRIDGE=YES bin/brogue-bridge.exe bin/brogue-bridge.dll
+    & $make $makePath "SHELL=$makeShell" SYSTEM=WINDOWS GRAPHICS=NO RELEASE=YES BRIDGE=YES bin/brogue-bridge.exe bin/brogue-bridge.dll
     if ($LASTEXITCODE -ne 0) {
         throw "Brogue bridge build failed with exit code $LASTEXITCODE."
     }
